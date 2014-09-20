e80fa335a2a40c395a8fadcb62a40e9e166ad58d
Buruc Asrin <b_asrin@hotmail.com>
2014-09-20 17:35:20 -0400

    working on open/close tabs in mic.onresult

--- scripts/recording.js
+++ scripts/recording.js
@@ -21,6 +21,47 @@ $(document).ready(function () {
     };
     mic.onresult = function (intent, entities) {
         // buruc mojo
+
+        //OPEN intention
+        if(intent == 'open'){
+            //gets all tabs with specified properties or all if no properties specified
+            chrome.tabs.query( { }, function(array_of_Tabs){
+
+                for (1 = 0; i < array_of_Tabs.length; i++){
+                    var tab = array_of_Tabs[i];
+                    //switch to this tab  
+                    if ((tab.url).contains(entities)){
+                        chrome.tabs.update(tab.tabId, {active: true});  
+                    }
+                    //create the new tab
+                    else{
+                        chrome.tabs.create('http://www.' + entities + '.com');
+                    }                  
+                }
+            }
+        }
+
+        //CLOSE intention
+        else{
+            //gets tabs with specified properties or all if no properties specified
+            chrome.tabs.query( { }, function(array_of_Tabs){
+
+                for (1 = 0; i < array_of_Tabs.length; i++){
+                    var tab = array_of_Tabs[i];
+                    //close this tab
+                    if ((tab.url).contains(entities)){
+                        chrome.tabs.remove(tab.tabId);  
+                    }
+                    //create the new tab
+                    else{
+                        break;//tab to be closed in fact does not exist, do nothing.
+                    }                  
+                }
+            }
+        }
+
+
+
     };
     mic.connect("CLIENT_T");
     // mic.start();
