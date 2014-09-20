bb6829965f79a6f7b133c6dedebdbf32315d5ad0
argo49 <argo_49@hotmail.com>
2014-09-20 03:52:50 -0400

    socket.io works!

--- cs.js
+++ cs.js
@@ -1,17 +1,13 @@
 window.onload = function () {
 
-    var scripts = [
-        'scripts/index.js',
-        'https://cdn.socket.io/socket.io-1.0.6.js'];
 
-    for (script in scripts) {
-        injectScript(script);
-    }
-
-}
-
-function injectScript(scriptPath) {
     var scriptContainer = document.createElement('script');
-    <scriptContainer.src = chrome.extension.getURL(scriptPath);
+    scriptContainer.src = chrome.extension.getURL('scripts/index.js');
+    document.getElementsByTagName('body')[0].appendChild(scriptContainer);
+
+    scriptContainer = null
+
+    scriptContainer = document.createElement('script');
+    scriptContainer.src = 'https://cdn.socket.io/socket.io-1.0.6.js';
     document.getElementsByTagName('body')[0].appendChild(scriptContainer);
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,26 @@
-var socket = io.connect('http://ancient-journey-3463.herokuapp.com/');
 
-// Emit another event back to the server
-socket.emit('my other event', { clientSide: 'data' });
+waitForSocketIO();
+
+function waitForSocketIO  () {
+    if (typeof io === 'undefined') {
+        console.log('waiting...');
+        window.setTimeout(waitForSocketIO, 10);
+    } else {
+        testSocketIO();
+    }
+}
+
+function testSocketIO () {
+    var socket = io.connect('http://ancient-journey-3463.herokuapp.com/');
+
+    // Emit another event back to the server
+    socket.emit('my other event', { clientSide: 'data' });
+    socket.on('news', function (data) {
+
+
+        console.log(data);
+
+        // Emit another event back to the server
+        socket.emit('my other event', { clientSide: 'data' });
+    });
+}
